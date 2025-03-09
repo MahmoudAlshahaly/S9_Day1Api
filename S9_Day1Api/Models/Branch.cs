bc322055836919cf7559f0dac69b69d5d1fa67e2
MahmoudAlshahaly <mahmoudahmed10197@gmail.com>
2025-03-09 20:40:07 +0200

    modify branch

--- S9_Day1Api/Models/Branch.cs
+++ S9_Day1Api/Models/Branch.cs
@@ -2,7 +2,8 @@
 {
     public class Branch
     {
-        public int Id    { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string  Address { get; set; }
     }
 }

842608a60c898a1875389a9e7cb893505caf7a43
MahmoudAlshahaly <mahmoudahmed10197@gmail.com>
2025-03-09 20:52:16 +0200

    some changes

--- S9_Day1Api/Models/Department.cs
+++ S9_Day1Api/Models/Department.cs
@@ -6,5 +6,8 @@
         public string Name{ get; set; }
         public string Location { get; set; }
         public ICollection<Employee>? Employees { get; set; }
+        //
+        //
+
     }
 }
